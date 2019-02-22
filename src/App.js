9c0e551908710233c7547c4ae3758e3c7a16f2fb
Kevin <kjh311@gmail.com>
2019-02-22 10:43:33 -0800

    starting

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import MyComponent from './MyComponent.js';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      title: 'app title'
+    }
+
+    this.onClick = this.onClick.bind(this);
+  }
+  
+  onClick(event){
+    this.setState({
+      title: 'New App Title'
+    });
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <h1>{this.state.title}</h1>
+        <div onClick={this.onClick}>Click here!</div>
+        <MyComponent 
+          title="This is the component title"
+          name="Kevin"
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/MyComponent.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class MyComponent extends Component {
+
+  render() {
+
+    return (
+      <div className="MyComponent">
+        <h1>'This is a simple component!'</h1>
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
